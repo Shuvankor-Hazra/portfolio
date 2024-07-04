7f21030fd8fbc6b26557e8a05a049953b91bacfa
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-07-04 19:55:53 +0600

    Project carocel added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <div name="About" className="bg-base-200 max-w-screen-2xl container mx-auto px-4 md:px-20 py-12 md:py-24">
                 <h1 className="text-4xl font-bold">About me</h1>
                 <p className="pt-3 pb-6 text-lg">Hello, i{"'"}am Shuvankor, a passionate Web developer with a keen eye  for MERN Stack</p>
-                <div className="space-y-3 border border-[#4B0082] p-12 rounded-2xl mt-5">
+                <div className="space-y-3 border border-[#4B0082] p-8 rounded-2xl mt-5">
                     <div>
                         <h2 className="text-lg font-bold text-[#4B0082]">Education & Training</h2>
                         <p>[ HSC, 2010, Dakshin Sreepur School & Collage ] [ SSC, 2008, Fatepur Secondary School ] [ Frontend web development, 2024, Programming Hero ] </p>
